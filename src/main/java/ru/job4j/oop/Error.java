5610c80732a392faab6fc172ec08bccc9aab14ed
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-04-04 21:56:05 +0200

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("active: " + active);
+        System.out.println("status: " + status);
+        System.out.println("message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        error1.printInfo();
+        Error error2 = new Error(true, 123, "error 2");
+        error2.printInfo();
+        Error error3 = new Error(false, 456, "error 3");
+        error3.printInfo();
+        Error error4 = new Error(true, 678, "error 4");
+        error4.printInfo();
+    }
+}
