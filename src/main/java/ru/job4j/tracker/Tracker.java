5d5fcc7e8940c02016d134412669e906dfb421e7
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-04-18 11:52:10 +0200

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,31 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        int counter = 0;
+        Item[] result = new Item[size];
+        for (int index = 0; index < size; index++) {
+            if (items[index] != null) {
+                result[index - counter] = items[index];
+            } else {
+                counter++;
+            }
+        }
+        return Arrays.copyOf(result, size - counter);
+    }
+
+    public Item[] findByName(String name) {
+        Item[] rsl = new Item[size];
+        int counter = 0;
+        for (int index = 0; index < size; index++) {
+            if (name.equals(items[index].getName())) {
+                rsl[counter] = items[index];
+                counter++;
+            }
+        }
+        return Arrays.copyOf(rsl, counter);
     }
 
     public Item findById(int id) {
