c90c48504f8e125102cd962171e23fccf9483d2d
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-04-18 19:10:55 +0200

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,12 +1,13 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
-
     private LocalDateTime created = LocalDateTime.now();
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() { }
 
@@ -17,6 +18,11 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name='" + name + '\'' + ", created=" + created.format(FORMATTER) + '}';
     }
 
     public int getId() {
@@ -38,5 +44,4 @@ public class Item {
     public LocalDateTime getCreated() {
         return created;
     }
-
 }
