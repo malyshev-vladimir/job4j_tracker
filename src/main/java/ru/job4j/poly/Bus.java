dff3461755e9fff1e405f79f4cce301e0229492f
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-04-23 07:59:52 +0200

    3. Интерфейс Input [#181092]. Добавление реализации методов класса Bus.

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -2,10 +2,14 @@ package ru.job4j.poly;
 
 public class Bus implements Transport, Vehicle {
     @Override
-    public void drive() { }
+    public void drive() {
+        System.out.println("Этот автобус едет куда-то по дороге");
+    }
 
     @Override
-    public void passengers(int passengers) { }
+    public void passengers(int passengers) {
+        System.out.println("Количество пассажиров равно " + passengers + " человек");
+    }
 
     @Override
     public double refuel(int fuelQuantity) {
