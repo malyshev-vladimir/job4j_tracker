0ee39cc77f0fe959ce776324bd8adc2a9326c7bf
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-04-25 03:50:10 +0200

    5.1. Тестирование. Подготовка данных. [#182960]. Исправления.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -47,10 +47,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
         tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                null
-        };
+        String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item edited = tracker.findById(item.getId());
         assertNull(edited);
