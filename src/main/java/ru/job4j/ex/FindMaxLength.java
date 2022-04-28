6e341d553101e9285b8be4a14af3b8824cef1250
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-04-28 21:26:18 +0200

    0.1. java.lang.NullPointerException [#219364]

---
+++ src/main/java/ru/job4j/ex/FindMaxLength.java
@@ -0,0 +1,14 @@
+package ru.job4j.ex;
+
+class FindMaxLength {
+    public static void main(String[] args) {
+        String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
+        int max = 0;
+        for (int index = 0; index < shops.length; index++) {
+            if (shops[index] != null && shops[index].length() > max) {
+                max = shops[index].length();
+            }
+        }
+        System.out.println("Max length : " + max);
+    }
+}
