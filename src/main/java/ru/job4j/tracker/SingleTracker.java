1b905b280b45de6d71e9b031304b18c9d5b7a147
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-05-12 17:50:01 +0200

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private SingleTracker instance = null;
+
+    private SingleTracker() {
+    }
+
+    public SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+
+    private Tracker tracker = new Tracker();
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findByName(String name) {
+        return tracker.findByName(name);
+    }
+}
