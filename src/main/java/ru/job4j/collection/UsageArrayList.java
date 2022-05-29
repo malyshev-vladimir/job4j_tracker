21a250b3d6d73cbc526136e7e78b2a2ccc43ac07
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-05-29 18:06:05 +0200

    0. Коллекции, ArrayList, List, Обобщения [#10094]. Code style fixes.

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 public class UsageArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
-
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
-
-        for (String name: names) {
+        for (String name : names) {
             System.out.println(name);
         }
     }
