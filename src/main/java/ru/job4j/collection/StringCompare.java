d94bafb317dd969bef42b0f46eaf8c488220717f
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-06-25 15:43:07 +0200

    3. Компаратор для строк [#35008 #315813]. Сhanges to simplify the class and reduce the amount of code.

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,25 +5,11 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int stringsLengthComparisonResult = Integer.compare(left.length(), right.length());
-        int characterComparisonResult;
-
-        if (stringsLengthComparisonResult <= 0) {
-            for (int index = 0; index < left.length(); index++) {
-                characterComparisonResult = Character.compare(left.charAt(index), right.charAt(index));
-                if (characterComparisonResult != 0) {
-                    return characterComparisonResult;
-                }
+        for (int index = 0; index < Math.min(left.length(), right.length()); index++) {
+            if (left.charAt(index) != right.charAt(index)) {
+                return Character.compare(left.charAt(index), right.charAt(index));
             }
-            return stringsLengthComparisonResult;
-        } else {
-            for (int index = 0; index < right.length(); index++) {
-                characterComparisonResult = Character.compare(left.charAt(index), right.charAt(index));
-                if (characterComparisonResult != 0) {
-                    return characterComparisonResult;
-                }
-            }
-            return stringsLengthComparisonResult;
         }
+        return Integer.compare(left.length(), right.length());
     }
 }
