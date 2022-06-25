5da71af4be093ac555a3bb4acc977e0ca5934682
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-06-25 15:46:58 +0200

    4. Сортировка номера [#221284 #315818]. Some changes.

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,6 +6,6 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        return Integer.parseInt(left.split(". ")[0]) - Integer.parseInt(right.split(". ")[0]);
+        return Integer.compare(Integer.parseInt(left.split(". ")[0]), Integer.parseInt(right.split(". ")[0]));
     }
 }
