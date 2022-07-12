1a0ce36387e0872b94dd61198d4c2a372e6e538a
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-07-12 23:06:04 +0200

    2. Список адресов [#110062].

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(profile -> profile.getAddress())
