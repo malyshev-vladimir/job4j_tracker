33d6d00635225426197924557752a5429d51d6f5
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-07-16 22:52:41 +0200

    5. Генерирование колоды карт. FlatMap [#504873].

---
+++ src/main/java/ru/job4j/stream/DoubleLoop.java
@@ -0,0 +1,11 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+public class DoubleLoop {
+    public static void main(String[] args) {
+        Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                        .map(value -> new Card(suit, value)));
+    }
+}
