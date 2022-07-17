69d9180be8f8905cda9b58d07cb5b9a134ef10da
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-07-17 14:34:38 +0200

    6. Преобразование матрицы чисел в список чисел. FlatMap. [#110228 #322856]. Removing the meaningless line and changing the lambda expression.

--- src/main/java/ru/job4j/stream/MatrixToList.java
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -1,7 +1,5 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,8 +8,7 @@ public class MatrixToList {
 
     public static List<Integer> convert(Integer[][] matrix) {
         return Stream.of(matrix)
-                .flatMap(list -> Stream.of(list)
-                        .map(number -> number))
+                .flatMap(Stream::of)
                 .collect(Collectors.toList());
     }
 }
