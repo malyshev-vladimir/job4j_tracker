d9d00105f09bc1de9bce0c2f91db661b60b3fd13
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-07-23 14:40:27 +0200

    2. Аттестация [#49777].

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,54 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.subjects().stream())
+                .mapToInt(subject -> subject.score())
+                .average()
+                .orElse(0);
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream
+                .map(pupil -> new Tuple(pupil.name(), Analyze.averageScore(Stream.of(pupil))))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(s -> s.name(), LinkedHashMap::new, Collectors.averagingDouble(Subject::score)))
+                .entrySet()
+                .stream()
+                .map(object -> new Tuple(object.getKey(), object.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream
+                .map(pupil -> new Tuple(pupil.name(), pupil.subjects()
+                        .stream()
+                        .mapToInt(subject -> subject.score())
+                        .sum()))
+                .max(Comparator.comparingDouble(tuple -> tuple.score()))
+                .get();
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.subjects().stream())
+                .collect(Collectors.groupingBy(subject -> subject.name(), LinkedHashMap::new, Collectors.summarizingDouble(Subject::score)))
+                .entrySet()
+                .stream()
+                .map(o -> new Tuple(o.getKey(), o.getValue().getSum()))
+                .max(Comparator.comparingDouble(tuple -> tuple.score()))
+                .get();
+    }
+}
